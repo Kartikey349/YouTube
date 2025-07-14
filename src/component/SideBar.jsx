640f4560641fede25eb8c3af36544091f2134ae1
kartikey sharma <kartikey7518@gmail.com>
2025-07-14 20:14:18 +0530

    created sideBar

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -1,7 +1,29 @@
 export const SideBar = () => {
     return(
-        <div>
-            SideBar
+        <div className="shadow-md w-50 p-4 flex flex-col gap-5">
+            <ul className="border-b-2 flex flex-col gap-2">
+                <li>Home</li>
+                <li>Shorts</li>
+                <li>Subscriptions</li>
+            </ul>
+
+            <ul className="border-b-2 flex flex-col gap-2">
+                <p className="font-bold">You &gt; </p>
+                <li>History</li>
+                <li>Playlists</li>
+                <li>Your Videos</li>
+                <li>Watch Later</li>
+                <li>Liked Video</li>
+            </ul>
+
+            <ul className="border-b-2 flex flex-col gap-2">
+                <p className="font-bold">Subscriptions &gt; </p>
+                <li>History</li>
+                <li>Playlists</li>
+                <li>Your Videos</li>
+                <li>Watch Later</li>
+                <li>Liked Video</li>
+            </ul>
         </div>
     )
 }
