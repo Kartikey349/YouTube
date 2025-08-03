66c6d7f005cd8f8ed90bcf23df2e7cad18077619
kartikey sharma <kartikey7518@gmail.com>
2025-08-04 01:59:40 +0530

    click to search categories

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,8 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { clearShowVideo, showVideo } from "../utils/searchVideoSlice";
+
 export const Button = (props) => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     const {name} = props;
+
+    const searchVideo = async (name) => {
+    
+            dispatch(clearShowVideo());
+    
+            if(name.length > 0){
+                const data = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${name}&maxResults=20&type=video&key=AIzaSyAqV-cVc1ohaRbxXo60zG3zKuJWHLKujeg`);
+                
+                
+                const json = await data.json();
+                
+                dispatch(showVideo(json.items))
+                navigate(`/result?search=${name}`)
+            }
+        }
+
     return(
         <div>
-           <button className="m-2 px-4 py-1 bg-slate-800 text-white rounded-md whitespace-nowrap">
+           <button className="m-2 px-4 py-1 bg-slate-800 text-white rounded-md whitespace-nowrap"
+           
+           onClick={() => {
+                searchVideo(name)
+           }}
+           >
             {name}
             </button>
         </div>
--- src/component/ButtonList.jsx
+++ src/component/ButtonList.jsx
@@ -1,12 +1,12 @@
 import { Button } from "./Button"
 
-const List = ["All", "gaming", "Sports","Weeknd","Musics","Movies","comedy","live","Asmr","Dnace"]
+const List = ["All", "gaming", "Sports","Weeknd","Musics","Movies","comedy","live","Asmr","Dance"]
 
 export const ButtonList = () => {
     return(
         <div className="flex overflow-x-auto w-full whitespace-nowrap pt-4">
             {List.map((item, index) => {
-               return <Button key={index} name={item} />
+               return <Button key={index} name={item} onclick />
             })}
         </div>
     )
